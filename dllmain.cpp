00406ae03ec33b56bdb6ca09232471d1ea6fcd82
Matthew <matthewtoo5@googlemail.com>
2021-10-29 03:13:01 +0100

    basic stuff

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,19 +1,39 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+
+
+FILE* fDummy;
+
+int WINAPI main()
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+#if defined _DEBUG
+	//Initialize Console
+	AllocConsole();
+	AttachConsole(GetCurrentProcessId());
+
+	//Redirect output to console
+
+	freopen_s(&fDummy, "CONIN$", "r", stdin);
+	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+	std::cout << "Welcome to Drinol!" << std::endl;
+#endif
+
+	//Do shit, etc hooks whatever
 }
 
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(nullptr, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(main), nullptr, NULL, nullptr);
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		//utils::dll_management::kill_dll();
+	}
+
+	return TRUE;
+}
--- pch.h
+++ pch.h
@@ -9,5 +9,7 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
+#include <cstdio>
+#include <iostream>
 
 #endif //PCH_H
