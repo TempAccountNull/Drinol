173a1b407a08510f175143456375ef0769b19a98
Matthew <matthewtoo5@googlemail.com>
2023-03-20 21:43:14 +0000

    Update halo2_hooks.h

--- games/halo2/halo2_hooks.h
+++ games/halo2/halo2_hooks.h
@@ -5,7 +5,7 @@ namespace halo2
 	class hooks
 	{
 	public:
-		inline static bool redirect_print = true;
+		inline static bool redirect_print; // WIP
 		static void init();
 		static void deinit();
 	};
