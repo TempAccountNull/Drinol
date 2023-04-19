b2558641fd7c7c6232b7fbd224beb8d0249fa86b
Matthew <matthewtoo5@googlemail.com>
2023-04-19 14:09:15 +0100

    Allow the console to be able to print out more colours.

--- console/console.cpp
+++ console/console.cpp
@@ -18,6 +18,27 @@ void console::init()
 	freopen_s(&fDummy, "CONIN$", "r", stdin);
 	freopen_s(&fDummy, "CONOUT$", "w", stderr);
 	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+
+	//https://stackoverflow.com/questions/75388829/how-to-get-the-output-console-screen-buffer-handle-for-getconsolemode-in-w
+	// Make console support more colors
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// Fetch existing console mode so we correctly add a flag and not turn off others
+	DWORD mode = 0;
+	if (!GetConsoleMode(hStdOut, &mode))
+	{
+		DWORD error = ::GetLastError();
+		MessageBox(NULL, L"Could not get console mode. : " + error, L"Drinol Error!", 0);
+	}
+
+	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+	// Try to set the mode.
+	if (!SetConsoleMode(hStdOut, mode))
+	{
+		DWORD error = ::GetLastError();
+		MessageBox(NULL, L"Could not set console mode. : " + error, L"Drinol Error!", 0);
+	}
 }
 
 void console::deinit()
