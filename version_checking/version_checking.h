c9fd65fa8f64e25dbdd49747ad7015546a0bd3e9
Jānis <97699850+janisslsm@users.noreply.github.com>
2023-09-08 17:09:31 +0300

    Halo Reach: update for MCC 1.3251.0.0

--- version_checking/version_checking.h
+++ version_checking/version_checking.h
@@ -2,7 +2,7 @@
 class version_checking
 {
 public:
-	static inline std::string COMPATIBLE_DRINOL_VERSION = "1.3073.0.0";
+	static inline std::string COMPATIBLE_DRINOL_VERSION = "1.3251.0.0";
 	static void validate();
 	static void validate_game_dll(std::string filename);
 };
